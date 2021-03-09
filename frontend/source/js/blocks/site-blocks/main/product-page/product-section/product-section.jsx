4b9011daa100c961005ab876ebda7f60b57b0701
PapkaRimskyi <39594926+PapkaRimskyi@users.noreply.github.com>
2021-03-09 15:50:54 +0300

    Request simulation. Loading icon. Redirect and wrong path listener

--- frontend/source/js/blocks/site-blocks/main/product-page/product-section/product-section.jsx
+++ frontend/source/js/blocks/site-blocks/main/product-page/product-section/product-section.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import { useLocation } from 'react-router-dom';
+
 import Filter from './filter/filter';
 import Products from './products/products';
 
 export default function ProductSection() {
+  const location = useLocation();
+
   return (
     <section className="row product-section">
       <h2 className="visually-hidden">Фильтры и список продуктов</h2>
       <Filter />
-      <Products />
+      <Products location={location.pathname} />
     </section>
   );
 }
--- frontend/source/js/blocks/universal/blocks/product-list/product-list.jsx
+++ frontend/source/js/blocks/universal/blocks/product-list/product-list.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import classNames from 'classnames';
+import LoadingIcon from '../../../svg/loading-icon/loading-icon';
 
 export default function ProductList({ majorClass, bootstrapClass, list }) {
+  const [loading, setLoading] = useState(null);
+  const [loadedData, setLoadedData] = useState(null);
+
+  // Симуляция запроса
+
+  useEffect(() => {
+    setLoading(true);
+    const timerID = setTimeout(() => {
+      new Promise((resolve) => resolve(list)).then((data) => {
+        setLoading((prevState) => !prevState);
+        setLoadedData(data);
+      });
+    }, 1000);
+
+    return () => clearTimeout(timerID);
+  }, [list]);
+
+  //
+
   return (
-    <ul className={classNames('product-list', majorClass, bootstrapClass)}>
-      {list.map((item, index) => (
-        <li key={index} className="product-list__item">
-          <figure className="product-list__product-img-container">
-            <img src={item.imgSrc} alt="Товар" />
-            <figcaption className="product-list__product-price">{`${item.price} руб`}</figcaption>
-          </figure>
-          <a href="test" className="link product-list__product-link">{item.name}</a>
-        </li>
-      ))}
-    </ul>
+    loading || !loadedData
+      ? <LoadingIcon />
+      : (
+        <ul className={classNames('product-list', majorClass, bootstrapClass)}>
+          {loadedData.map((item) => (
+            <li key={item.id} className="product-list__item">
+              <figure className="product-list__product-img-container">
+                <img src="assets/img/product-placeholder.png" alt="Товар" />
+                <figcaption className="product-list__product-price">{`${item.price} руб`}</figcaption>
+              </figure>
+              <a href="test" className="link product-list__product-link">{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      )
   );
 }
 
